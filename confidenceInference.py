cc5ad45debbef455a7f823950a75ec83c58982bb
aktiger <aktiger.zc@antfin.com>
2023-04-19 18:17:51 +0800

    nsig and prob

--- confidenceInference.py
+++ confidenceInference.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+from scipy.stats import norm
 
 def calPriceIntervalPositive(initPrice, N_sigma, volatility, leftTime, yearDay):
     return initPrice * np.exp(N_sigma * volatility * np.sqrt(leftTime / yearDay))
@@ -9,11 +10,24 @@ def calPriceIntervalNegative(initPrice, N_sigma, volatility, leftTime, yearDay):
     return initPrice * np.exp(-N_sigma * volatility * np.sqrt(leftTime / yearDay))
 
 
-def fullInterval(initPrice, N_sigma, volatility, leftTime, yearDay):
+def fullIntervalWithNSigma(initPrice, N_sigma, volatility, leftTime, yearDay):
+    return "[" + str(calPriceIntervalNegative(initPrice, N_sigma, volatility, leftTime, yearDay)) + "," + \
+           str((calPriceIntervalPositive(initPrice, N_sigma, volatility, leftTime, yearDay))) + "]"
+
+
+def fullIntervalWithProb(initPrice, prob, volatility, leftTime, yearDay):
+    N_sigma = norm.ppf(1- (1-prob)/2)
     return "[" + str(calPriceIntervalNegative(initPrice, N_sigma, volatility, leftTime, yearDay)) + "," + \
            str((calPriceIntervalPositive(initPrice, N_sigma, volatility, leftTime, yearDay))) + "]"
 
 
 if __name__ == '__main__':
-    print(fullInterval(float(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]),float(sys.argv[4]),float(sys.argv[5])))
+    if (sys.argv[1]=="nsig"):
+        print(fullIntervalWithNSigma(float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]),
+                                 float(sys.argv[6])))
+    elif(sys.argv[1]=="prob"):
+        print(fullIntervalWithProb(float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]),
+                                 float(sys.argv[6])))
+    else:
+        print("error input")
 
