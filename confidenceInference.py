27ac3f1e410f6daf107705f8949f36d63dd73dd1
aktiger <aktiger.zc@antfin.com>
2023-04-19 18:24:38 +0800

    example

--- confidenceInference.py
+++ confidenceInference.py
@@ -21,6 +21,15 @@ def fullIntervalWithProb(initPrice, prob, volatility, leftTime, yearDay):
            str((calPriceIntervalPositive(initPrice, N_sigma, volatility, leftTime, yearDay))) + "]"
 
 # python confidenceInference.py Prob 5600 0.86638 0.21 30 252 22
+
+'''
+ ~/cvs/option_tools  master  python confidenceInference.py prob 5600 0.9973002039367398 0.21 30 252                           ✔  base   18:22:36
+[4505.937387484188,6959.706117334515]
+
+ ~/cvs/option_tools  master  python confidenceInference.py nsig 5600 3 0.21 30 252                                            ✔  base   18:23:29
+[4505.937387484187,6959.7061173345155]
+'''
+
 if __name__ == '__main__':
     if (sys.argv[1]=="nsig"):
         print(fullIntervalWithNSigma(float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]),
