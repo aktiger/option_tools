4a9c2b234f43bcc709e6a8e67ca42f349730a3f7
aktiger <aktiger.zc@antfin.com>
2023-05-04 23:18:05 +0800

    5.4 data add

--- confidenceInference.py
+++ confidenceInference.py
@@ -1,21 +1,24 @@
 import numpy as np
 import sys
-from scipy.stats import norm
 
-def calPriceIntervalPositive(initPrice, N_sigma, volatility, leftTime, yearDay):
+import scipy.stats
+from scipy.stats import norm
+from scipy.stats import lognorm
+
+def calPriceIntervalPositive(initPrice, N_sigma, volatility, leftTime, yearDay=252):
     return initPrice * np.exp(N_sigma * volatility * np.sqrt(leftTime / yearDay))
 
 
-def calPriceIntervalNegative(initPrice, N_sigma, volatility, leftTime, yearDay):
+def calPriceIntervalNegative(initPrice, N_sigma, volatility, leftTime, yearDay=252):
     return initPrice * np.exp(-N_sigma * volatility * np.sqrt(leftTime / yearDay))
 
 
-def fullIntervalWithNSigma(initPrice, N_sigma, volatility, leftTime, yearDay):
+def fullIntervalWithNSigma(initPrice, N_sigma, volatility, leftTime, yearDay=252):
     return "[" + str(calPriceIntervalNegative(initPrice, N_sigma, volatility, leftTime, yearDay)) + "," + \
            str((calPriceIntervalPositive(initPrice, N_sigma, volatility, leftTime, yearDay))) + "]"
 
 
-def fullIntervalWithProb(initPrice, prob, volatility, leftTime, yearDay):
+def fullIntervalWithProb(initPrice, prob, volatility, leftTime, yearDay=252):
     N_sigma = norm.ppf(1- (1-prob)/2)
     return "[" + str(calPriceIntervalNegative(initPrice, N_sigma, volatility, leftTime, yearDay)) + "," + \
            str((calPriceIntervalPositive(initPrice, N_sigma, volatility, leftTime, yearDay))) + "]"
@@ -34,9 +37,11 @@ if __name__ == '__main__':
     if (sys.argv[1]=="nsig"):
         print(fullIntervalWithNSigma(float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]),
                                  float(sys.argv[6])))
+
     elif(sys.argv[1]=="prob"):
         print(fullIntervalWithProb(float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]),
                                  float(sys.argv[6])))
+
     else:
         print("error input " + sys.argv[1])
 
